53c80bc8182b831c0684febcbe6390a1b3499c6f
Prashant Tiwari <prashant@zondax.ch>
2021-10-04 16:34:07 +0530

    added igw, gateway attachment and ALB

--- infrastructure.py
+++ infrastructure.py
@@ -1,26 +1,45 @@
-import troposphere.ec2 as ec2
 import troposphere.elasticloadbalancingv2 as elb
 
+
 from troposphere import (
-    Ref,
-    Template,
-    Tags,
+    Base64,
+    FindInMap,
     GetAtt,
-    Join
+    Join,
+    Output,
+    Parameter,
+    Ref,
+    Tags,
+    Template,
 )
-
+from troposphere.autoscaling import Metadata
+from troposphere.cloudformation import (
+    Init,
+    InitConfig,
+    InitFile,
+    InitFiles,
+    InitService,
+    InitServices,
+)
 from troposphere.ec2 import (
+    EIP,
+    VPC,
+    Instance,
+    InternetGateway,
+    NetworkAcl,
+    NetworkAclEntry,
+    NetworkInterfaceProperty,
+    PortRange,
     Route,
-    VPCGatewayAttachment, 
-    SubnetRouteTableAssociation, 
-    Subnet, 
-    RouteTable, 
-    VPC, 
-    SubnetNetworkAclAssociation, 
-    EIP, 
-    InternetGateway
+    RouteTable,
+    SecurityGroup,
+    SecurityGroupRule,
+    Subnet,
+    SubnetNetworkAclAssociation,
+    SubnetRouteTableAssociation,
+    VPCGatewayAttachment,
 )
-
+from troposphere.policies import CreationPolicy, ResourceSignal
 
 t = Template()
 
@@ -30,50 +49,97 @@ vpc = t.add_resource(
         'VPC',
         CidrBlock="10.0.0.0/16",
         Tags=Tags(
-            Name="My VPC"
+            Name="My-VPC"
         )
     )
 )
 
 
-# Create a public subnet
-public_subnet = t.add_resource(
+# Create a public subnet 0
+public_subnet_0 = t.add_resource(
     Subnet(
-        "PublicSubnet",
-        VpcId=Ref(VPC),
+        "PublicSubnet0",
+        VpcId=Ref(vpc),
         CidrBlock="10.0.10.0/24",
         MapPublicIpOnLaunch=True,
         AvailabilityZone=Join("", [Ref("AWS::Region"), "a"]),
         Tags=Tags(
-            Name="My public subnet"
+            Name="My-public-subnet-0"
         )
     )
 )
 
-
-# Create a private subnet
-public_subnet = t.add_resource(
+# Create a public subnet 1
+public_subnet_1 = t.add_resource(
     Subnet(
-        "PrivateSubnet",
-        VpcId=Ref(VPC),
+        "PublicSubnet1",
+        VpcId=Ref(vpc),
         CidrBlock="10.0.20.0/24",
+        MapPublicIpOnLaunch=True,
+        AvailabilityZone=Join("", [Ref("AWS::Region"), "b"]),
+        Tags=Tags(
+            Name="My-public-subnet-1"
+        )
+    )
+)
+
+# Create a private subnet 0
+private_subnet_0 = t.add_resource(
+    Subnet(
+        "PrivateSubnet0",
+        VpcId=Ref(vpc),
+        CidrBlock="10.0.50.0/24",
         AvailabilityZone=Join("", [Ref("AWS::Region"), "a"]),
         Tags=Tags(
-            Name="My private subnet"
+            Name="My-private-subnet-0"
         )
     )
 )
 
-
+# Create a private subnet 2
+private_subnet_1 = t.add_resource(
+    Subnet(
+        "PrivateSubnet1",
+        VpcId=Ref(vpc),
+        CidrBlock="10.0.60.0/24",
+        AvailabilityZone=Join("", [Ref("AWS::Region"), "b"]),
+        Tags=Tags(
+            Name="My-private-subnet-0"
+        )
+    )
+)
 # Create internet gateway
 internet_gateway = t.add_resource(
     InternetGateway(
         "InternetGateway",
+        Tags=Tags(
+            Name="My-internet-gateway"
+        )    
     )
 )
 
-# Create a ALB
-ALB = t.add_resoource()
+
+# Attach internet gateway with VPC
+gateway_attachment = t.add_resource(
+    VPCGatewayAttachment(
+        "VPCGatewayAttachment",
+        VpcId=Ref(vpc),
+        InternetGatewayId=Ref(internet_gateway)
+    ) 
+)
+
+# Create ALB
+alb = t.add_resource(
+    elb.LoadBalancer(
+        "ALB",
+        Scheme="internet-facing",
+        Subnets=[Ref(public_subnet_0), Ref(public_subnet_1)],
+        Tags=Tags(
+            Name="My-ALB"
+        )
+    )
+)
+
 
 
 print(t.to_yaml())
