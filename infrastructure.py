84da6c7739a6f33274f7a0f5f54fa28b38d03666
Prashant Tiwari <prashant@zondax.ch>
2021-10-07 06:55:08 +0530

    Initial commit with Network, Network Subnet and Ubuntu server

--- infrastructure.py
+++ infrastructure.py
@@ -28,6 +28,7 @@ from troposphere.ec2 import (
     VPC,
     Instance,
     InternetGateway,
+    NatGateway,
     NetworkAcl,
     NetworkAclEntry,
     NetworkInterfaceProperty,
@@ -120,6 +121,42 @@ private_subnet_1 = t.add_resource(
     )
 )
 
+# Create internet gateway
+internet_gateway = t.add_resource(
+    InternetGateway(
+        "InternetGateway",
+        Tags=Tags(
+            Name="My-internet-gateway"
+        )    
+    )
+)
+
+
+# Attach internet gateway with VPC
+gateway_attachment = t.add_resource(
+    VPCGatewayAttachment(
+        "VPCGatewayAttachment",
+        VpcId=Ref(vpc),
+        InternetGatewayId=Ref(internet_gateway)
+    ) 
+)
+
+
+# Nat Gateway EIP
+nat_gateway_EIP = t.add_resource(EIP(
+    "NatGatewayEIP",
+    Domain = "vpc"
+))
+
+
+# Create Nat Gateway
+nat_gateway = t.add_resource(NatGateway(
+    "NatGateway",
+    AllocationId = GetAtt(nat_gateway_EIP, 'AllocationId'),
+    SubnetId = Ref(public_subnet_0)
+))
+
+
 # Create security group for public subnet
 public_subnet_sg = t.add_resource(
     ec2.SecurityGroup(
@@ -137,6 +174,12 @@ public_subnet_sg = t.add_resource(
                 IpProtocol="tcp",
                 FromPort="80",
                 ToPort="80",
+                CidrIp="0.0.0.0/0",
+            ),
+            ec2.SecurityGroupRule(
+                IpProtocol="tcp",
+                FromPort="8080",
+                ToPort="8080",
                 CidrIp="0.0.0.0/0",
             ),
         ],
@@ -166,26 +209,55 @@ private_subnet_sg = t.add_resource(
     )
 )
 
-
-# Create internet gateway
-internet_gateway = t.add_resource(
-    InternetGateway(
-        "InternetGateway",
-        Tags=Tags(
-            Name="My-internet-gateway"
-        )    
+# Create Public Route Table
+public_route_table = t.add_resource(RouteTable(
+    "PublicRouteTable",
+    VpcId = Ref(vpc),
+    Tags = Tags(
+        Name = "Public-Route-Table"
     )
-)
+))
+
+# Default Public Route for Public Route Table
+t.add_resource(Route(
+    "DefaultPublicRoute",
+    DependsOn = gateway_attachment.title,
+    RouteTableId = Ref(public_route_table),
+    DestinationCidrBlock = "0.0.0.0/0",
+    GatewayId = Ref(internet_gateway)
+))
+
+# Public Subnet Route Table Association
+t.add_resource(SubnetRouteTableAssociation(
+    "PublicSubnetRouteTableAssociation",
+    RouteTableId = Ref(public_route_table),
+    SubnetId = Ref(public_subnet_0)
+))
 
 
-# Attach internet gateway with VPC
-gateway_attachment = t.add_resource(
-    VPCGatewayAttachment(
-        "VPCGatewayAttachment",
-        VpcId=Ref(vpc),
-        InternetGatewayId=Ref(internet_gateway)
-    ) 
-)
+# Private Route Table
+private_route_table = t.add_resource(RouteTable(
+    "PrivateRouteTable",
+    VpcId = Ref(vpc),
+    Tags = Tags(
+        Name = "Private-Route-Table"
+    )
+))
+
+# Default Private Route
+t.add_resource(Route(
+    "DefaultPrivateRoute",
+    RouteTableId = Ref(private_route_table),
+    DestinationCidrBlock = "0.0.0.0/0",
+    NatGatewayId = Ref(nat_gateway)
+))
+
+# Private Subnet Route Table Association
+t.add_resource(SubnetRouteTableAssociation(
+    "PrivateSubnetRouteTableAssociation",
+    RouteTableId = Ref(private_route_table),
+    SubnetId = Ref(private_subnet_0)
+))
 
 # Create ALB
 alb = t.add_resource(
@@ -198,6 +270,7 @@ alb = t.add_resource(
         )
     )
 )
+
 
 # Create EC2 instance
 ec2_instance = t.add_resource(
@@ -220,19 +293,18 @@ ec2_instance = t.add_resource(
 )
 
 
-
 # Create Target Groups
 target_group = t.add_resource(
     elb.TargetGroup(
         "TargetGroup",
-        HealthCheckIntervalSeconds="30",
+        HealthCheckIntervalSeconds="300",
         HealthCheckProtocol="HTTP",
         HealthCheckTimeoutSeconds="10",
         HealthyThresholdCount="4",
         Matcher=elb.Matcher(
             HttpCode="200"),
         Name="MyTargetSopara1337",
-        Port="8080",
+        Port="80",
         Protocol="HTTP",
         Targets=[elb.TargetDescription(
             Id=Ref(ec2_instance),
@@ -256,5 +328,6 @@ alb_listener = t.add_resource(
         )]
     )
 )
+
 
 print(t.to_yaml())
